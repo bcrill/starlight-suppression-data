40a62a2c8c51888d0b53f4cc1f25fd2109f9358c
Brendan Crill <bcrill@gmail.com>
2023-04-11 11:29:22 -0700

    adding suppression curve data

---
+++ py/contrastplot.py
@@ -0,0 +1,163 @@
+import matplotlib.pyplot as plt
+from astropy.table import Table
+import numpy as np
+
+version = "2023.03.01"
+output_plot_file = f"Contrast_lab_demos_{version}.png"
+output_readme_file = f"README_Contrast_lab_demos_{version}.txt"
+
+
+fnames = [
+    "Seo_et_al_fig3_digitized.csv",
+    "VVC_650nm_oneBMCDM_clearPupil.csv",
+    "starshade_ms1b.csv",
+    "VVC_635nm_twoAOXDMs_clearPupil_v2.csv",
+    "PIAACMC_650nm_Belikov2022_BB.csv",
+    "VVC_650nm_oneBMCDM_segPupil.csv",
+]
+
+
+# prepare readme text
+readme = f"The plot {output_plot_file} shows reported lab demonstrations of starlight suppression technology.\n\n"
+
+# prepare to collect references
+references = []
+
+# prepare to plot
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(1, 1, 1)
+
+# loop over filenames
+for fname in fnames:
+    T = Table.read(f"../data/contrast_curves/{fname}", format="ascii.ecsv")
+
+    if T.meta["coronagraph"] == "starshade":
+        labelstr = f"{T.meta['testbed']}\n"
+    else:
+        labelstr = (
+            f"{T.meta['testbed']} {T.meta['pupil']} pupil {T.meta['coronagraph']}\n"
+        )
+    labelstr += f"{T.meta['wl']}nm ({T.meta['reference']}) {T.meta['bp_pct']}% band"
+
+    ax.plot(
+        T["x"],
+        T["y"],
+        color=T.meta["color"],
+        ls=T.meta["ls"],
+        label=labelstr,
+    )
+
+    readmestr = (
+        f"* {T.meta['color']} "
+        + {"-": "solid curve", "--": "dashed curve", ":": "dotted curve"}[T.meta["ls"]]
+        + " : \n"
+    )
+    if T.meta["coronagraph"] != "starshade":
+        readmestr += f"demonstration of {T.meta['coronagraph']} coronagraph with a {T.meta['pupil']} pupil "
+        readmestr += f"using {T.meta['DM']} deformable mirror(s) for wavefront control "
+    else:
+        readmestr += f"demonstration of {T.meta['coronagraph']} "
+    readmestr += (
+        f"at {T.meta['wl']}nm over a {T.meta['bp_pct']}% band ({T.meta['reference']}). "
+    )
+    if "courtesy" in T.meta.keys():
+        readmestr += "Data courtesy of " + T.meta["courtesy"] + "."
+    readmestr += "\n"
+    if "note" in T.meta.keys():
+        readmestr += T.meta["note"] + "\n"
+    readmestr += "\n"
+
+    # add reference
+    if "reference" in T.meta.keys():
+        refstr = T.meta["reference"] + " " + T.meta["url"]
+        if refstr not in references:
+            references.append(refstr)
+    readme += readmestr
+
+# add version watermark
+plt.text(
+    0.02,
+    0.01,
+    f"Version {version}",
+    horizontalalignment="left",
+    alpha=0.5,
+    fontsize=8,
+    transform=plt.gca().transAxes,
+)
+
+plt.loglog()
+ax.set_xlim(0.9, 50)
+ax.set_ylim(0.7e-12, 1.1e-7)
+
+ax.set_ylabel("Normalized intensity")
+ax.set_xlabel("Angle [$\lambda$/D]")
+
+
+ax.axhspan(1e-13, 1e-10, alpha=0.1, facecolor="k")
+
+plt.legend(fontsize=7, loc="lower right")
+
+plt.text(
+    11,
+    7e-11,
+    "Contrast performance goal $\leq$10$^{-10}$",
+    horizontalalignment="left",
+    alpha=0.8,
+    fontsize=8,
+)
+ax.axvline(x=3.0, ls="--", color="k", alpha=0.3)
+plt.text(
+    2.7,
+    1e-9,
+    "Inner working angle goal $\leq$3$\lambda$/D",
+    alpha=0.9,
+    fontsize=7,
+    rotation="vertical",
+)
+ax.axvline(x=45, ls="--", color="k", alpha=0.3)
+plt.text(
+    41,
+    1e-9,
+    "Outer working angle goal $\geq$45$\lambda$/D",
+    alpha=0.8,
+    fontsize=7,
+    rotation="vertical",
+)
+
+
+# Add additional axis
+ax2 = ax.twiny()
+
+lDtomas = 640e-9 / 6 * 180 / np.pi * 3600 * 1000
+
+
+# convert from lambda/D to milliarcseconds on a 6 meter and 640 nm
+def tick_function(X):
+    V = X * lDtomas
+    return ["%.f" % z for z in V]
+
+
+ax2.set_xlim(ax.get_xlim())
+
+new_tick_locations = np.array([50.0, 500.0, 1000.0]) / lDtomas
+
+
+ax2.set_xticks(new_tick_locations)
+ax2.set_xticklabels(tick_function(new_tick_locations))
+ax2.set_xlabel("Angle [mas] on a 6m telescope at 640 nm")
+
+
+plt.suptitle("Starlight suppression broadband lab demonstrations")
+plt.savefig(output_plot_file)
+
+
+with open(output_readme_file, "w") as f:
+    f.write(readme)
+
+    f.write("\nReferences\n")
+    f.write("----------\n")
+    for reference in references:
+        f.write(f"* {reference}\n")
+    f.write("\n")
+
+    f.close()
